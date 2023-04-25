d7a4ab292bd8a5851826d36df6c81ec56564ca7c
Bilal Mehmoood <bmehmood199@gmail.com>
2023-04-25 10:21:40 +0500

    Moved to hostinger

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(167.71.22.235 167.71.22.235)
+domains=(grafana1.tkturners.in grafana1.tkturners.in)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
