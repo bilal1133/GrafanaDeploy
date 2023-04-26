5b048c48144f411611ab246c3f656333a08c277d
Bilal Mehmoood <bmehmood199@gmail.com>
2023-04-26 19:25:09 +0500

    senifone

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(grafana1.tkturners.in grafana1.tkturners.in)
+domains=(ase.senifone.com ase.senifone.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
--- setup.sh
+++ setup.sh
@@ -2,22 +2,8 @@ echo "### Setting up docker."
 
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt-key add -
 sudo apt update
-apt-cache policy docker-ce
-sudo apt install docker-ce
-sudo systemctl status docker
-
-echo "### Setting up docker compose."
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-echo "### creating grafana data directory."
-
-mkdir -p "data/grafana"
-mkdir -p "/var/lib/grafana"
 
 echo "### initialize lets encrypt."
 sudo chmod +x ./init-letsencrypt.sh
